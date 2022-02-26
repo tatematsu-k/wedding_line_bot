7cde57124d66341c77c30a046fd17edd9f97ff79
tatematsu-k <ynu1264@gmail.com>
2022-02-26 17:29:34 +0900

    add rubocop and refix for rubocop

--- spec/requests/webhook/lines_controller_spec.rb
+++ spec/requests/webhook/lines_controller_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 describe Webhook::LinesController do
-  describe 'POST /webhook/line' do
+  describe "POST /webhook/line" do
     subject { post webhook_line_path }
 
     it { is_expected.to eq 200 }
