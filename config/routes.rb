c7e41e0fe0ef3e73ce1739cc8da53a45e6a16e2f
tatematsu-k <ynu1264@gmail.com>
2022-03-12 17:49:22 +0900

    rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resource :line, only: [:create]
   end
 
-  get 'healthcheck', to: proc { [200, {}, ['']] }
+  get "healthcheck", to: proc { [200, {}, [""]] }
 end
