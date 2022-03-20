778a27df572539dbe8694dc099c5591023051331
tatematsu-k <ynu1264@gmail.com>
2022-03-20 18:13:44 +0900

    refix for heroku

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-bundle exec rails db:create
-bundle exec ridgepole -E production -c config/database.yml --apply
+rm -f /myapp/tmp/pids/server.pid
+# bundle exec rails db:create
+# bundle exec ridgepole -E production -c config/database.yml --apply
 
 exec "$@"
