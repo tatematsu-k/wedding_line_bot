e1f9a4adf37e02b21d5a176eca6795e2d7a4e66f
tatematsu-k <ynu1264@gmail.com>
2022-03-21 17:38:46 +0900

    add columns for question status on line_users

--- db/schema/line_users.rb
+++ db/schema/line_users.rb
@@ -3,6 +3,7 @@
 create_table :line_users do |t|
   t.string :line_uid, null: false, index: { unique: true }
   t.string :follow_status, null: false
+  t.string :question_status, null: false
 
   t.timestamps
 end
